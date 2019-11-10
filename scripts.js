78ea05e9aac1f26f68e6616c3d97c88b7e787890
Emerald Teve <emerald.teve@gmail.com>
2019-11-09 19:39:50 -0600

    Concept complete!

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
+function darkmode() {
+    document.getElementById('dnc').style.color = "white";
+    document.getElementById('time').style.color = "white";
+    document.getElementById('wrapper').style.background = "#0C181D";
+}
+
 function lightmode() {
-    document.getElementById('lm').style.background = "#0C181D";
-    document.getElementById('lm-img').src = "img/sun-dm.png";
-    document.getElementsByClassName('wrapper').background = "#0C181D";
-    document.getElementsByTagName('h1').color = "white";
-    document.getElementsByTagName('h3').color = "#0C181D";
+    document.getElementById('dnc').style.color = "#0C181D";
+    document.getElementById('time').style.color = "#0C181D";
+    document.getElementById('wrapper').style.background = "white";
 }
