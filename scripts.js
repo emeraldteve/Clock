9e4fe5997444a26e6132709697f8518cd33980b6
Emerald Teve <emerald.teve@gmail.com>
2019-11-09 19:08:07 -0600

    Structured page according to self.

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function lightmode() {
+    document.getElementById('lm').style.background = "#0C181D";
+    document.getElementById('lm-img').src = "img/sun-dm.png";
+    document.getElementsByClassName('wrapper').background = "#0C181D";
+    document.getElementsByTagName('h1').color = "white";
+    document.getElementsByTagName('h3').color = "#0C181D";
+}
