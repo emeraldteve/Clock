6b07ca121f9fe3574d5ac0681690461a2f464500
Emerald Teve <emerald.teve@gmail.com>
2019-11-09 20:19:44 -0600

    : ^)

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 function darkmode() {
     document.getElementById('dnc').style.color = "white";
     document.getElementById('time').style.color = "white";
-    document.getElementById('wrapper').style.background = "#0C181D";
+    document.getElementById('container').style.background = "#0C181D";
 }
 
 function lightmode() {
     document.getElementById('dnc').style.color = "#0C181D";
     document.getElementById('time').style.color = "#0C181D";
-    document.getElementById('wrapper').style.background = "white";
+    document.getElementById('container').style.background = "white";
 }
